37d1e42b455f1b9644fbe95e337204f20f8f9da1
Lucie RYST <lryst@e1r11p14.42.fr>
2021-03-19 13:44:51 +0100

    philo test + norme OK

--- philo_one/philo_actions.c
+++ philo_one/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 19:08:45 by lryst             #+#    #+#             */
-/*   Updated: 2021/03/17 13:37:00 by lryst            ###   ########.fr       */
+/*   Updated: 2021/03/19 13:42:40 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	philo_think(t_philo *philo)
 {
 	if (philo->status == 3)
 	{
-		printf("%ldms %d is thinking\n", (get_time() - philo->top), philo->i);
+		printf("%ldms %d is thinking\n", (get_time() - philo->top),
+		philo->i + 1);
 		philo->status = 1;
 	}
 }
@@ -28,7 +29,8 @@ void	philo_sleep(t_philo *philo)
 	if (philo->status == 2)
 	{
 		chrono = get_time();
-		printf("%ldms %d is sleeping\n", (get_time() - philo->top), philo->i);
+		printf("%ldms %d is sleeping\n", (get_time() - philo->top),
+		philo->i + 1);
 		usleep(philo->sleep * 1000);
 		philo->status = 3;
 	}
@@ -61,10 +63,10 @@ void	philo_eat(t_philo *philo)
 		take_fork(philo);
 		philo->l_chrono = get_time();
 		printf("%ldms %d has taken a fork\n",
-		(get_time() - philo->top), philo->i);
+		(get_time() - philo->top), philo->i + 1);
 		printf("%ldms %d has taken a fork\n",
-		(get_time() - philo->top), philo->i);
-		printf("%ldms %d is eating\n", (get_time() - philo->top), philo->i);
+		(get_time() - philo->top), philo->i + 1);
+		printf("%ldms %d is eating\n", (get_time() - philo->top), philo->i + 1);
 		philo->r_turn += 1;
 		if (philo->r_turn == philo->turn)
 			philo->nbr_turn = 1;
